2c4e46f711b2cbed53e38d87cb6a12851e054bf5
Mert patlar <meertpatlar@gmail.com>
2021-12-10 22:33:29 +0300

    validation layer adding (not all done)

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { listResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModule } from '../models/ResponseModel';
 
 
 @Injectable({
@@ -23,4 +24,7 @@ export class ProductService {
     return this.httpclient.get<listResponseModel<Product>>(newPath);
   }
 
+  add(product:Product):Observable<ResponseModule>{
+    return this.httpclient.post<ResponseModule>(this.apiUrl+"products/add",product); 
+  }
 }
